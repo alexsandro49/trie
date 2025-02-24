39bd26ca2d2506a715d1ba177f1575f155dcda93
Alex Sandro <alexsandro49.dev@gmail.com>
2025-02-24 20:49:07 -0300

    feat: collect added words

--- trie.go
+++ trie.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type Trie struct {
 	Root *Node
 }
@@ -33,4 +35,27 @@ func (t *Trie) Add(word string) {
 	}
 
 	current.IsLeaf = true
+}
+
+func (t *Trie) collectAllWords() []string {
+	words := []string{}
+
+	t.collectAllWordsHelper(t.Root, &words, strings.Builder{})
+
+	return words
+}
+
+func (t *Trie) collectAllWordsHelper(node *Node, words *[]string, prefix strings.Builder) {
+	if node.IsLeaf {
+		*words = append(*words, prefix.String())
+	}
+
+	for k, v := range node.Children {
+		newPrefix := strings.Builder{}
+
+		newPrefix.WriteString(prefix.String())
+		newPrefix.WriteString(string(k))
+
+		t.collectAllWordsHelper(v, words, newPrefix)
+	}
 }
