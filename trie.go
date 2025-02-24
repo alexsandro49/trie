e02fc5844b09cded5757bec24718c1fc2a82b566
Alex Sandro <alexsandro49.dev@gmail.com>
2025-02-24 17:28:12 -0300

    feat: add a word

--- trie.go
+++ trie.go
@@ -3,3 +3,34 @@ package main
 type Trie struct {
 	Root *Node
 }
+
+func NewTrie() Trie {
+	return Trie{
+		&Node{
+			make(map[rune]*Node),
+			false,
+		},
+	}
+}
+
+func (t *Trie) Add(word string) {
+	current := t.Root
+
+	for _, v := range word {
+		_, ok := current.Children[v]
+
+		if ok {
+			current = current.Children[v]
+
+		} else {
+			current.Children[v] = &Node{
+				make(map[rune]*Node),
+				false,
+			}
+
+			current = current.Children[v]
+		}
+	}
+
+	current.IsLeaf = true
+}
